2626963ccc369fe819af809bc3302cb785bbb62f
Abdullah Al Zubaer <zubaerislam703@gmail.com>
2026-02-17 06:39:57 +0600

    updated

---
+++ cmd/migrate/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "modernc.org/sqlite"                            // SQLite driver without CGO
+	_ "github.com/golang-migrate/migrate/v4/source/file" // <-- this registers the file source
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/sqlite"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide a migration direction: 'up' or 'down'")
+	}
+
+	direction := os.Args[1]
+
+	db, err := sql.Open("sqlite", "./data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	driver, err := sqlite.WithInstance(db, &sqlite.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://cmd/migrate/migrations", // migration folder
+		"sqlite3",
+		driver,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch direction {
+	case "up":
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	case "down":
+		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	default:
+		log.Fatal("Invalid migration direction. Use 'up' or 'down'.")
+	}
+
+	log.Println("Migration completed successfully!")
+}
